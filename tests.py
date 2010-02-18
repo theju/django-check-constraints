55b8c04d1f5fa372ab6e3dc18064339d56644563
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2010-02-18 21:50:55 +0530

    Fixed bugs in tests

--- tests.py
+++ tests.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
+from django.core.management.color import no_style
+from django.db import connection
+from datetime import datetime
+from check_constraints_app.models import CCTestModel
 from check_constraints import *
+
+opts = CCTestModel._meta
 
 class CheckConstraintTestCase(TestCase):
     def testNoKwargArgPassed(self):
@@ -9,22 +15,56 @@ class CheckConstraintTestCase(TestCase):
         x = lambda : Check(price_gte=10)
         self.assertRaises(SyntaxError, x)
 
-    def testVerifyCheckName(self):
-        c = Check(price__gte = 10)
-        c.check_name = "check_price"
-        gen_sql_stmt = c.generate_sql()
-        sql_stmt = """CONSTRAINT "check_price" CHECK ("price" >= 10) """
-        self.assertEquals(gen_sql_stmt, sql_stmt)
-
     def testIsCascaded(self):
         c = Check(price__gte=0) & Check(price__gte='discount')
         self.assertEquals(c.is_cascaded, True)
 
+    def testSQLGen1(self):
+        c = Check(price__gte = 10)
+        c.check_name = "check_price"
+        c.validate(opts)
+        gen_sql_stmt = c.generate_sql(connection, no_style)
+        sql_stmt = """CONSTRAINT "check_price" CHECK ( "price" >= 10 )"""
+        self.assertEquals(gen_sql_stmt, sql_stmt)
+
+    def testSQLGen2(self):
+        c = Check(name__like__upper = 'THEJ%')
+        c.check_name = "check_name"
+        c.validate(opts)
+        gen_sql_stmt = c.generate_sql(connection, no_style)
+        sql_stmt = """CONSTRAINT "check_name" CHECK ( UPPER("name") like 'THEJ%' )"""
+        self.assertEquals(gen_sql_stmt, sql_stmt)
+
+    def testSQLGen3(self):
+        c = Check(discount__between = [10, 20])
+        c.check_name = "check_discount"
+        c.validate(opts)
+        gen_sql_stmt = c.generate_sql(connection, no_style)
+        sql_stmt = """CONSTRAINT "check_discount" CHECK ( "discount" between ( 10,20 ) )"""
+        self.assertEquals(gen_sql_stmt, sql_stmt)
+
+    def testSQLGen4(self):
+        c = Check(gender__in = ("Male", "Female"))
+        c.check_name = "check_gender"
+        c.validate(opts)
+        gen_sql_stmt = c.generate_sql(connection, no_style)
+        sql_stmt = """CONSTRAINT "check_gender" CHECK ( "gender" in ( 'Male','Female' ) )"""
+        self.assertEquals(gen_sql_stmt, sql_stmt)
+
+    def testSQLGen5(self):
+        c = Check(mfg_date__gt = datetime(2010,1,1))
+        c.check_name = "check_mfg_date"
+        c.validate(opts)
+        gen_sql_stmt = c.generate_sql(connection, no_style)
+        sql_stmt = """CONSTRAINT "check_mfg_date" CHECK ( "mfg_date" > '2010-01-01 00:00:00' )"""
+        self.assertEquals(gen_sql_stmt, sql_stmt)
+
     def testCascadedSQLGen(self):
         c = Check(price__gte=0) & Check(price__gte='discount') | Check(price__lte=100)
         c.check_name = "check_name_price"
-        gen_sql_stmt = c.generate_sql()
-        sql_stmt = """CONSTRAINT "check_name_price" CHECK ( ("price" >= 0) AND ("price" >= discount) OR ("price" <= 100) )"""
+        c.validate(opts)
+        gen_sql_stmt = c.generate_sql(connection, no_style)
+        sql_stmt = """CONSTRAINT "check_name_price" CHECK ( ( "price" >= 0 ) AND ( "price" >= discount ) OR ( "price" <= 100 ) )"""
         self.assertEquals(gen_sql_stmt, sql_stmt)
 
     def testInvalidLookupArg(self):
@@ -32,7 +72,8 @@ class CheckConstraintTestCase(TestCase):
         self.assertRaises(KeyError, x)
 
     def testInvalidFieldLookup(self):
-        x = lambda : Check(tax_percentage__lt=10)
+        c = Check(tax_percentage__lt=10)
+        x = lambda : c.validate(opts)
         self.assertRaises(NonExistentFieldError, x)
 
 
